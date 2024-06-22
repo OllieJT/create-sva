26fa77a72e10ecff7cf632c411db777ea733c860
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2024-06-22 15:09:35 +0100

    Setup Linting

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -6,11 +6,32 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
+	alias: { $src: 'src', $lib: 'src/lib' },
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+	},
+	env: {
+		publicPrefix: 'PUBLIC_',
+	},
+
+	typescript: {
+		config: (/** @type {{ compilerOptions: Record<string,any>; }} */ initial) => ({
+			...initial,
+			compilerOptions: {
+				...initial.compilerOptions,
+				allowJs: true,
+				checkJs: true,
+				esModuleInterop: true,
+				forceConsistentCasingInFileNames: true,
+				resolveJsonModule: true,
+				skipLibCheck: true,
+				sourceMap: true,
+				strict: true,
+			},
+		}),
 	},
 };
 
