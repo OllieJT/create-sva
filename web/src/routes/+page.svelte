d20c5e104c0e31aeb93df3ed06b6e5fa679d5cd8
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2024-06-22 13:26:46 +0100

    move docs repo

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -2,6 +2,6 @@
 	import { MyCounterButton } from '@repo/ui';
 </script>
 
-<h1>Docs</h1>
+<h1>Web</h1>
 <MyCounterButton />
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
