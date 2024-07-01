cbe49ac34c657ca6caab620453856a2a86ea70cb
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2024-07-01 16:51:13 +0100

    Fix Drizzle postgres config

--- cli/template/extras/config/drizzle-config-postgres.ts
+++ cli/template/extras/config/drizzle-config-postgres.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
 	schema: "./src/lib/server/schema.ts",
 	out: "./migrations",
-	dialect: "postgres",
+	dialect: "postgresql",
 
 	dbCredentials: {
 		url: process.env.DATABASE_URL!,
